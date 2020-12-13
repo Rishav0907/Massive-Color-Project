cfb955943c799f97f31ef7e34345910bd65e0333
Rishav <rishav@localhost.localdomain>
2020-12-13 12:47:27 +0530

    slider and navbar added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react'
 import  Pallete from "./Pallete";
 import seedColors from "./seedColors";
-
+import { generatePallete } from "./colorHelpers";
 class App extends Component{
   render(){
+    console.log(generatePallete(seedColors[4]))
     // console.log(seedColors[4])
     return (
       <div className="App">
-        <Pallete {...seedColors[4]} />
+        <Pallete pallete={generatePallete(seedColors[4])} />
       </div>
     );
   }
--- src/Pallete.jsx
+++ src/Pallete.jsx
@@ -1,15 +1,30 @@
 import React, { Component } from 'react';
 import ColorBox from "./ColorBox";
 import './Pallete.css'
-class Pallete extends Component{
-    render(){
-        let colorBoxes=this.props.colors.map(color =>(
-            <ColorBox background={color.color} name={color.name} />
+import Navbar from './Navbar'
+class Pallete extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            level: 500
+        }
+        this.changeLevel=this.changeLevel.bind(this)
+    }
+    changeLevel(newLevel){
+        this.setState({
+            level:newLevel
+        })
+    }
+    render() {
+        let {colors}=this.props.pallete
+        let {level}=this.state;
+        let colorBoxes = colors[level].map(color => (
+            <ColorBox background={color.hex} name={color.name}  />
         ))
         console.log(this.props.colors);
-        return(
-
+        return (
             <div className="Pallete">
+                <Navbar level={level} changeLevel={this.changeLevel} />
                 <div className="Pallete-colors">
                     {colorBoxes}
                 </div>
