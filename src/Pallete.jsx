cafd61e72f2f960d3177186b1a8fb5a1d66ac500
Rishav <rishav@localhost.localdomain>
2020-12-18 13:55:46 +0530

    color format added

--- src/Pallete.jsx
+++ src/Pallete.jsx
@@ -6,25 +6,33 @@ class Pallete extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            level: 500
+            level: 500,
+            format:'hex'
         }
         this.changeLevel=this.changeLevel.bind(this)
+        this.changeFormat=this.changeFormat.bind(this)
     }
     changeLevel(newLevel){
         this.setState({
             level:newLevel
         })
     }
+    changeFormat(val){
+        this.setState({
+            format:val
+        })
+    }
     render() {
         let {colors}=this.props.pallete
-        let {level}=this.state;
+        let {level,format}=this.state;
+        console.log({format});
         let colorBoxes = colors[level].map(color => (
-            <ColorBox background={color.hex} name={color.name}  />
+            <ColorBox background={color[format]} name={color.name}  />
         ))
         console.log(this.props.colors);
         return (
             <div className="Pallete">
-                <Navbar level={level} changeLevel={this.changeLevel} />
+                <Navbar level={level} changeLevel={this.changeLevel} handleChange={this.changeFormat} />
                 <div className="Pallete-colors">
                     {colorBoxes}
                 </div>
